78e593d080673008db5bedf5802cbeb57c6b3082
fish895623 <dan990429@gmail.com>
2025-03-18 01:52:30 +0900

    feat: Receive post methods

--- src/main/java/com/github/fish895623/tasks/controller/ProjectController.java
+++ src/main/java/com/github/fish895623/tasks/controller/ProjectController.java
@@ -2,13 +2,24 @@ package com.github.fish895623.tasks.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/projects")
+@Slf4j
 public class ProjectController {
     @GetMapping
     public String getProjects() {
         return "projects";
+    }
+
+    @PostMapping
+    public String addProject(@RequestParam(required = true, name = "title") String title) {
+        log.info("addProject: {}", title);
+        return "redirect:/projects";
     }
 }
