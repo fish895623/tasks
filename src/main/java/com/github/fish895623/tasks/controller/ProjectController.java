3698851e6be40cbdeab2f802ddc27aa371bfdbe4
fish895623 <dan990429@gmail.com>
2025-03-18 01:48:04 +0900

    add project controller

---
+++ src/main/java/com/github/fish895623/tasks/controller/ProjectController.java
@@ -0,0 +1,14 @@
+package com.github.fish895623.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+    @GetMapping
+    public String getProjects() {
+        return "projects";
+    }
+}
