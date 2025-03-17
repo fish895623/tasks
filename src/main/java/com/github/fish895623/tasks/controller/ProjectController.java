41b86f81459dcae5d977d6ca286745e97e2989cb
fish895623 <dan990429@gmail.com>
2025-03-18 02:50:55 +0900

    feat: Enhance projects page with AJAX functionality for adding projects and dynamically loading project list

--- src/main/java/com/github/fish895623/tasks/controller/ProjectController.java
+++ src/main/java/com/github/fish895623/tasks/controller/ProjectController.java
@@ -2,36 +2,46 @@ package com.github.fish895623.tasks.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.fish895623.tasks.entity.ProjectEntity;
-import com.github.fish895623.tasks.service.ProjectService;
+import com.github.fish895623.tasks.repository.ProjectRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
+@Slf4j
+@RestController
 public class ProjectController {
-    private final ProjectService projectService;
+    private final ProjectRepository projectRepository;
 
     @GetMapping
     public List<ProjectEntity> getProjects() {
-        return projectService.getProjects();
+        log.info("getProjects");
+        return projectRepository.findAll();
     }
 
     @PostMapping
     public ProjectEntity addProject(@RequestBody ProjectEntity project) {
-        projectService.addProject(project.getTitle());
-
-        return project;
+        log.info("addProject: {}", project);
+        return projectRepository.save(project);
     }
 
     @GetMapping("/{id}")
     public ProjectEntity getProjectById(@PathVariable Long id) {
-        return projectService.getProjectById(id);
+        return projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Project not found"));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProject(@PathVariable Long id) {
+        projectRepository.deleteById(id);
     }
 }
--- src/main/java/com/github/fish895623/tasks/view/TodoViewController.java
+++ src/main/java/com/github/fish895623/tasks/view/TodoViewController.java
@@ -1,7 +1,6 @@
 package com.github.fish895623.tasks.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +19,7 @@ public class TodoViewController {
     private final TodoService todoService;
 
     @GetMapping
-    public String getAllTodos(Model model) {
-        model.addAttribute("todos", todoService.getAllTodos());
+    public String getAllTodos() {
         return "index";
     }
 
