dd8584b2e84659d78c50175fef10222780c171db
fish895623 <dan990429@gmail.com>
2025-03-19 00:56:31 +0900

    feat: Add functionality to retrieve todos by project ID in TodoController and TodoService

--- src/main/java/com/github/fish895623/tasks/repository/TodoRepository.java
+++ src/main/java/com/github/fish895623/tasks/repository/TodoRepository.java
@@ -1,10 +1,14 @@
 package com.github.fish895623.tasks.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.github.fish895623.tasks.entity.ProjectEntity;
 import com.github.fish895623.tasks.entity.TodoEntity;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+    List<TodoEntity> findByProject(ProjectEntity project);
 }
--- src/main/java/com/github/fish895623/tasks/service/TodoService.java
+++ src/main/java/com/github/fish895623/tasks/service/TodoService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.github.fish895623.tasks.entity.ProjectEntity;
 import com.github.fish895623.tasks.entity.TodoEntity;
+import com.github.fish895623.tasks.repository.ProjectRepository;
 import com.github.fish895623.tasks.repository.TodoRepository;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -14,9 +16,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepository;
+    private final ProjectRepository projectRepository;
 
     public List<TodoEntity> findAll() {
         return todoRepository.findAll();
+    }
+
+    public List<TodoEntity> findByProjectId(Long projectId) {
+        ProjectEntity project = projectRepository.findById(projectId)
+                .orElseThrow(() -> new EntityNotFoundException("Project not found with id: " + projectId));
+
+        return todoRepository.findByProject(project);
     }
 
     public TodoEntity getTodoById(Long id) {
